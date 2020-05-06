dad2712b7114601ee5af0bd330e9f7d662cf4ad5
Rafael Mancini <rafael.lmancini@gmail.com>
2020-05-06 17:16:07 -0300

    Add basic configuration files to allow development

---
+++ bin/docker_entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+echo "=== MAPFRY APP ENTRYPOINT ==="
+
+COMMAND="$1"
+
+case "$COMMAND" in
+  server)
+    echo "=== RUNNING SERVER ==="
+    yarn start
+    ;;
+  test)
+    echo "=== RUNNING TEST SUITE ==="
+    yarn test
+    ;;
+  lint)
+    echo "=== RUNNING LINTER ==="
+    yarn lint
+    ;;
+  bundle)
+    echo "=== RUNNING LINTER ==="
+    yarn install --check-files
+    ;;
+  *)
+    echo "=== RUNNING COMAND  $*==="
+    sh -c "$*"
+    ;;
+esac
