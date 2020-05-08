d104e4b0a1e797299a8146950d0491a764dc6d96
Rafael Mancini <rafael.lmancini@gmail.com>
2020-05-08 17:40:59 -0300

    Add main styles and layout in index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import Router from './components/common/router'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'; // CSS Bootstrap
+import Header from './components/common/Header'
+import Home from './components/home/Home'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Blog from './components/blog/Blog'
+import './App.css'
+
 
 function App() {
   return (
-    <Router/>
+    <Router>
+      <Header/>
+      <Switch>
+        <Route path='/' exact component={Home}></Route>
+        <Route path='/about' exact component={About}></Route>
+        <Route path='/contact' exact component={Contact}></Route>
+        <Route path='/blog' exact component={Blog}></Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/root.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
